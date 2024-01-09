6a6c2fa6e2e05435ebbff0d791eb52338544fbcd
dangtong76 <dangtong@gmail.com>
2024-01-09 17:27:25 +0900

    init

--- 00-Create-Cluster/cloudWave-200-VPC.tf
+++
@@ -1,4 +0,0 @@
-data "aws_vpc" "cwave-vpc" {
-  id = var.cwave-vpc-id
-}
-
---
+++ cloudwave/terraform-olive/variables.tf
@@ -0,0 +1,37 @@
+#########################################################################################################
+## Terraform configurations (AWS)
+#########################################################################################################
+#variable "aws_access_key" {
+#  type        = string
+#  description = "AWS Access Key"
+#}
+#
+#variable "aws_secret_key" {
+#  type        = string
+#  description = "AWS Secret Key"
+#}
+#
+#variable "aws_session_token" {
+#  type        = string
+#  description = "AWS Session Token"
+#}
+
+variable "pem_location" {
+  type    = string
+  default = "."
+}
+
+
+#########################################################################################################
+## EKS Variable
+#########################################################################################################
+
+variable "cluster-name" {
+  description = "AWS kubernetes cluster name"
+  default     = "wave"
+}
+
+variable "cluster-version" {
+  description = "AWS EKS supported Cluster Version to current use"
+  default     = "1.27"
+}
